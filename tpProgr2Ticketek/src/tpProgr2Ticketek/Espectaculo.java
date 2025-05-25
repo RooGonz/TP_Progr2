2f1b5f2d797401682bd769826375ead4ed2a4a48
Carolina Gota <88778272+CaroGota@users.noreply.github.com>
2025-05-25 17:17:37 -0300

    Update Espectaculo.java
    Espectáculo desde cero

--- tpProgr2Ticketek/src/tpProgr2Ticketek/Espectaculo.java
+++ tpProgr2Ticketek/src/tpProgr2Ticketek/Espectaculo.java
@@ -1,5 +1,56 @@
 package tpProgr2Ticketek;
 
-public class Espectaculo {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-}
+public class Espectaculo {
+    private int codigo;
+    private String nombre;
+    private List<Funcion> funciones;
+ 
+
+    // Constructor 
+     public crearEspectaculo(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.funciones = new ArrayList<>();
+    }
+
+    public void agregarFuncion(Funcion funcion) {
+        funciones.add(funcion);
+    }
+
+    public List<String> verTodasLasSedes() {                                      //devuelve lista con todas las funciones
+        Set<String> sedes = new HashSet<>();                                      // Usamos Set para evitar repetidos, no es un map porque no me sirve de nada terne clave y dato
+        for (Funcion f : funciones) {
+            for (Sede s : f.getSedes()) {
+                sedes.add(s.getNombre());
+            }
+        }
+        return new ArrayList<>(sedes);
+    }
+
+    public double totalVendido() {
+        double total = 0;                                    //recorre totdas las funciones y y por cada funcion las entradas vendidas
+        for (Funcion f : funciones) {                        // y suma el preci0 final de todas las entradas
+            for (Entrada e : f.getEntradasVendidas()) {
+                total += e.getPrecioFinal();
+            }
+        }
+        return total;
+    }
+
+// Getters y  setters
+    public List<Funcion> getFunciones() {
+        return funciones;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
